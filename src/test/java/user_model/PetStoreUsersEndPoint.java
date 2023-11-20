d47cd96a9a0cb70c89cb79afd7b90dc59b8be3be
Andrii Bolshakov <12asic12@gmail.com>
2023-11-20 14:58:27 +0200

    the first init

---
+++ src/test/java/user_model/PetStoreUsersEndPoint.java
@@ -0,0 +1,40 @@
+package user_model;
+
+import com.google.gson.Gson;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+public class PetStoreUsersEndPoint {
+
+    public Response getUserByUsername(String username) {
+        return given()
+                .baseUri(Config.PETSTORE_BASE_URL)
+                .pathParam("username", username)
+                .when()
+                .get(Config.USER_BY_USERNAME)
+                .then().extract().response();
+    }
+
+    public Response createUser(User user) {
+        Gson gson = new Gson();
+        String json;
+        json = gson.toJson(user);
+
+        return given()
+                .baseUri(Config.PETSTORE_BASE_URL)
+                .contentType("application/json")
+                .body(json)
+                .when()
+                .post(Config.CREATE_USER)
+                .then().extract().response();
+    }
+
+    public Response deleteUser(String username) {
+        return given()
+                .baseUri(Config.PETSTORE_BASE_URL)
+                .when()
+                .delete(Config.USER_BY_USERNAME, username)
+                .then().extract().response();
+    }
+}
---
+++ src/test/java/user_model/User.java
@@ -0,0 +1,117 @@
+package user_model;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private int userStatus;
+
+    public int getID() {
+        return id;
+    }
+
+//    public void setID(int ID) {
+//        this.id = ID;
+//    }
+//
+//    public String getUserName() {
+//        return username;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.username = userName;
+//    }
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public int getUserStatus() {
+//        return userStatus;
+//    }
+//
+//    public void setUserStatus(int userStatus) {
+//        this.userStatus = userStatus;
+//    }
+
+    public User(int id, String username, String firstName, String lastName, String email, String password, String phone, int userStatus) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.userStatus = userStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && userStatus == user.userStatus && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(phone, user.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, firstName, lastName, email, password, phone, userStatus);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", phone='" + phone + '\'' +
+                ", userStatus=" + userStatus +
+                '}';
+    }
+}
